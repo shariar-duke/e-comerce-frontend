6e4723351c58d1a65e06fe79b623318488ae2721
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:56:15 +0600

    initial Main.jsx component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import Home from './home/Home'
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './user/Login';
+import Register from './user/Register';
 export default function Main() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </div>
