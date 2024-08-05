021e9823322fe54d41238f9e56d24e8130f6fc57
shariar <shariarmahmud225@gmail.com>
2024-08-05 07:28:53 +0600

    main.jsx file created

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import { Switch, Route, Redirect } from 'react-router-dom'
+export default function Main() {
+    return (
+        <div className="container mt-5">
+
+        </div>
+    )
+}
