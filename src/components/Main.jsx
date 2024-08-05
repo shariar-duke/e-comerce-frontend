010ed71fbed2caad064571137d1f952a436c1ebe
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:43:05 +0600

    style updated in main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Home from './home/Home'
 import { Routes, Route, Navigate } from 'react-router-dom';
 export default function Main() {
     return (
-        <div className="container mt-5">
+        <div>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
