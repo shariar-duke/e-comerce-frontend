571dabf60973a69cc014817bcedd4d0980054330
shariar <shariarmahmud225@gmail.com>
2024-08-05 07:27:54 +0600

    route applided in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-
+import Main from "./components/Main";
+import { BrowserRouter } from "react-router-dom"
 function App() {
   return (
-    <div className="container mt-5">
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="https://via.placeholder.com/150" className="card-img-top" alt="Card Image" />
-        <div className="card-body">
-          <h5 className="card-title">Card Title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card content.
-          </p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter >
+      <Main />
+    </BrowserRouter>
   );
 }
 
