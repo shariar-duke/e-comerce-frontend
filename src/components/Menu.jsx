5595dbace11fa26814cdb9ad7b560e8f1146fb30
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:28:40 +0600

    Menu/nvavbar component created

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+export default function Menu() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <Link className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link">Register</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
