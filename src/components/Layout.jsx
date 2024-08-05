ebb81c0322e6c5dd6b2e0dded879c03a75e89a8c
shariar <shariarmahmud225@gmail.com>
2024-08-05 07:42:01 +0600

    Layout file updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react"
+import { useEffect } from "react";
+
 export default function Layout({ title = "Title", className, children }) {
 
     useEffect(() => {
-        document.title = title
-    }, [])
+        document.title = title;
+    }, [title]);
+
     return (
-        <div >
+        <div>
             <div className="mb-3">
                 <h3>Menu</h3>
             </div>
-
             <div className={className}>
                 {children}
             </div>
         </div>
-    )
+    );
 }
