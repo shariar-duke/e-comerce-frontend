ab1f7d6a77741189726453f7c63896453851fdfd
shariar <shariarmahmud225@gmail.com>
2024-08-05 07:41:24 +0600

    Home.jsx file updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,10 @@
-import Layout from "../Layout"
+import Layout from "../Layout";
+
 export default function Home() {
   return (
     <Layout title="Home Page" className="container">
-
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the content of the Home component.</p>
     </Layout>
-  )
+  );
 }
