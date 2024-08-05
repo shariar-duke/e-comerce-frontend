d7d8d194c2f6f971fcdb9d22194933068535d155
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:56:05 +0600

    initail Registration component

---
+++ src/components/user/Register.jsx
@@ -0,0 +1,9 @@
+import Layout from "../Layout"
+export default function Register() {
+    return (
+        <Layout title="Login" className="container col-md-8 offset-md-2">
+            <h3>Register Here</h3>
+            <hr />
+        </Layout>
+    )
+}
