480d6c0d4d567d84e311a77ca1e9fc77cd0a0234
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:42:07 +0600

    Menu component implemented

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect } from "react";
+import Menu from "./Menu";
 
 export default function Layout({ title = "Title", className, children }) {
 
@@ -10,7 +11,8 @@ export default function Layout({ title = "Title", className, children }) {
     return (
         <div>
             <div className="mb-3">
-                <h3>Menu</h3>
+                {/* Menu compoent implemented */}
+                <Menu />
             </div>
             <div className={className}>
                 {children}
