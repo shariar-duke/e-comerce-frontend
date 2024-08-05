f04d72288b65a986868ec2f8df113889233519a2
shariar <shariarmahmud225@gmail.com>
2024-08-05 08:58:00 +0600

    right link to the navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 export default function Menu() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <ul className="nav nav-tabs">
                 <li className="nav-item">
-                    <Link className="nav-link">Home</Link>
+                    <Link to="/home" className="nav-link">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link">Login</Link>
+                    <Link to="/login" className="nav-link">Login</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link">Register</Link>
+                    <Link to="/register" className="nav-link">Register</Link>
                 </li>
             </ul>
         </nav>
