fd51ffdc52f47a4d2551434351c766037788f047
shariar <shariarmahmud225@gmail.com>
2024-08-06 21:11:54 +0600

    Register component updated

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,22 +1,47 @@
 import Layout from "../Layout";
-
+import { useState } from "react";
+import { showError, showLoading } from "../../utils/messages.jsx"
 export default function Register() {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: false,
+        loading: false,
+        disabled: false,
+        success: false,
+    })
+    const { name, email, password, success, error, loading, disabled } = values;
+
+    // handleChnage function er jnno
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            error: false,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("The Data I should got is", values)
+    }
     return (
         <Layout title="Register" className="container col-md-8 offset-md-2">
             <h3>Register Here</h3>
             <hr />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" id="name" placeholder="Enter your name" required />
+                    <input type="text" onChange={handleChange} className="form-control" id="name" name="name" placeholder="Enter your name" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" placeholder="Enter your email" required />
+                    <input type="email" onChange={handleChange} className="form-control" id="email" name="email" placeholder="Enter your email" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Enter your password" required />
+                    <input type="password" onChange={handleChange} className="form-control" id="password" name="password" placeholder="Enter your password" required />
                 </div>
                 <button type="submit" className="btn btn-primary mt-3">Register</button>
             </form>
