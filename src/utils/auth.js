f3c8239bc7ba3fc908ffbe7aea18c9f164d17e0a
shariar <shariarmahmud225@gmail.com>
2024-08-07 08:22:50 +0600

    function to check authenticate user

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,3 +9,20 @@ export const authenticate = (token, cb) => {
 // if(typeof window !=="undefined")  etar mnae holo kaj gula sob browser theke kora hoitse naki ... jode browser theke hy tahle egula kaj hbe
 
 // localStore.setItem('jwt', JSON.stringify(token)) // er mane holo jwt key er under a token ta rakhlam
+
+
+// ekhn ekta function lekhbo authentication er jnno ...
+
+export const isAuthenticated = () => {
+    if (typeof window == "undefined") {
+        return false;
+    }
+
+    else if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem(''))
+    }
+
+    else return false;
+}
+
+//
