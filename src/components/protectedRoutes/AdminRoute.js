cdf7226002adf222c6dd071598ee0e32321eda2e
shariar <shariarmahmud225@gmail.com>
2024-08-07 12:27:49 +0600

    path changed

--- src/components/protectedRoutes/AdminRoute.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-import { Navigate } from "react-router-dom";
-import { isAuthenticated, userInfo } from "../../utils/auth";
-
-export default function AdminPrivateRoute({ element: Element, ...rest }) {
-    const { role } = userInfo()
-    return isAuthenticated() && role === "admin" ? <Element {...rest} /> : <Navigate to="/login" />
-}
