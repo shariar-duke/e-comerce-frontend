1928b84f2ae541c8fcb0c09261fdfe15355ed820
shariar <shariarmahmud225@gmail.com>
2024-08-10 02:44:00 +0600

    create category component created

---
+++ src/components/admin/createCategory.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import Layout from '../Layout';
+import { showError, showSuccess } from '../../utils/messages';
+import { Link } from 'react-router-dom';
+
+const CreateCategory = () => {
+    const [values, setValues] = useState({
+        name: '',
+        error: false,
+        success: false
+    });
+
+    const { name, error, success } = values;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = (e) => {
+        setValues({
+            ...values, [e.target.name]: e.target.value
+        })
+    }
+
+    const categoryForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="text-muted">Name</label>
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={handleChange}
+                        value={name}
+                        autoFocus
+                        required
+                        className="form-control"
+                    />
+                </div>
+                <button type="submit" className="btn btn-outline-primary">Create Category</button>
+            </form>
+        )
+    }
+
+    const goBack = () => (<div className="mt-5">
+        <Link to="/admin/dashboard" className="text-warning">Go to Dashboard</Link>
+    </div>)
+
+
+    return (
+        <Layout title="Add a new category" description="Ready to add a new category?">
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showError(error, error)}
+                    {showSuccess(success, 'Category Created!')}
+                    {categoryForm()}
+                    {goBack()}
+                </div>
+            </div>
+        </Layout>
+    );
+
+}
+
+export default CreateCategory;
